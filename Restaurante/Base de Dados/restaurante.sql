7279c2e59bb91245370a69e4209e6fd21973734a
Adolfo Monteiro Manuel <adolfomonteiromanuel@gmail.com>
2024-04-05 23:09:56 +0100

    .
    Update

--- Restaurante/Base de Dados/restaurante.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: select *from cad_funcionarios
-LIMIT 0, 1000
-
--- Date: 2024-03-25 19:21
-*/
-INSERT INTO `` (`id`,`nome`,`telefone`,`email`,`morada`,`senha`) VALUES (3,'Leonardo Luyeye',921376488,'leonardoluyeye20@gmail.com','Cazenga/Mabor','123');
-INSERT INTO `` (`id`,`nome`,`telefone`,`email`,`morada`,`senha`) VALUES (4,'Adolfo Monteiro',928549260,'adolfomonteiromanuel@gmail.com','Viana','123456789');
